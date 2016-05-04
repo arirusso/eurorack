d51828a004c1d0400d6078d3d793883bb0bffe18
Ari Russo <ari.russo@gmail.com>
2016-05-04 00:33:38 -0400

    remove unused filter

--- peaks/noise/white.cc
+++ peaks/noise/white.cc
@@ -13,14 +13,9 @@ namespace peaks {
 
 using namespace stmlib;
 
-void WhiteNoiseGenerator::Init() {
-  noise_.Init();
-  noise_.set_frequency(105 << 7);  // 8kHz
-  noise_.set_resonance(24000);
-  noise_.set_mode(SVF_MODE_HP);
-}
+void WhiteNoise::Init() { }
 
-int16_t WhiteNoiseGenerator::ProcessSingleSample(uint8_t control) {
+int16_t WhiteNoise::ProcessSingleSample(uint8_t control) {
 
   int16_t noise = Random::GetSample();
 
--- peaks/noise/white.h
+++ peaks/noise/white.h
@@ -1,33 +1,28 @@
 //
 
-#ifndef PEAKS_WHITE_NOISE_GENERATOR_H_
-#define PEAKS_WHITE_NOISE_GENERATOR_H_
+#ifndef PEAKS_WHITE_NOISE_H_
+#define PEAKS_WHITE_NOISE_H_
 
 #include "stmlib/stmlib.h"
-
-#include "peaks/drums/svf.h"
 
 #include "peaks/gate_processor.h"
 
 namespace peaks {
 
-class WhiteNoiseGenerator {
+class WhiteNoise {
  public:
-  WhiteNoiseGenerator() { }
-  ~WhiteNoiseGenerator() { }
+  WhiteNoise() { }
+  ~WhiteNoise() { }
 
   void Init();
   int16_t ProcessSingleSample(uint8_t control) IN_RAM;
   void Configure(uint16_t* parameter, ControlMode control_mode) { }
 
  private:
-  Svf noise_;
 
-  uint32_t phase_[6];
-
-  DISALLOW_COPY_AND_ASSIGN(WhiteNoiseGenerator);
+  DISALLOW_COPY_AND_ASSIGN(WhiteNoise);
 };
 
 }  // namespace peaks
 
-#endif  // PEAKS_WHITE_WHITE_NOISE_GENERATOR_H_
+#endif  // PEAKS_WHITE_NOISE_H_

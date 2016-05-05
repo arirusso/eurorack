e77fa14a72798f2378fa9a684da3b5010c41ed87
Ari Russo <ari.russo@gmail.com>
2016-05-05 19:48:02 -0400

    get ui working with all three noise functions

--- peaks/processors.h
+++ peaks/processors.h
@@ -8,10 +8,10 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,7 +19,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-// 
+//
 // See http://creativecommons.org/licenses/MIT/ for more information.
 //
 // -----------------------------------------------------------------------------
@@ -53,16 +53,13 @@
 namespace peaks {
 
 enum ProcessorFunction {
-  PROCESSOR_FUNCTION_ENVELOPE,
-  PROCESSOR_FUNCTION_LFO,
-  PROCESSOR_FUNCTION_TAP_LFO,
+  PROCESSOR_FUNCTION_WHITE_NOISE,
+  PROCESSOR_FUNCTION_PINK_NOISE,
+  PROCESSOR_FUNCTION_HH_NOISE,
   PROCESSOR_FUNCTION_BASS_DRUM,
   PROCESSOR_FUNCTION_SNARE_DRUM,
   PROCESSOR_FUNCTION_HIGH_HAT,
   PROCESSOR_FUNCTION_FM_DRUM,
-  PROCESSOR_FUNCTION_HH_NOISE,
-  PROCESSOR_FUNCTION_PINK_NOISE,
-  PROCESSOR_FUNCTION_WHITE_NOISE,
   PROCESSOR_FUNCTION_PULSE_SHAPER,
   PROCESSOR_FUNCTION_PULSE_RANDOMIZER,
   PROCESSOR_FUNCTION_BOUNCING_BALL,
@@ -94,51 +91,51 @@ enum ProcessorFunction {
     variable.Configure(p, control_mode); \
   } \
   ClassName variable;
-  
+
 
 class Processors {
  public:
   Processors() { }
   ~Processors() { }
-  
+
   void Init(uint8_t index);
-  
-  typedef void (Processors::*InitFn)(); 
-  typedef int16_t (Processors::*ProcessSingleSampleFn)(uint8_t); 
-  typedef void (Processors::*FillBufferFn)(); 
+
+  typedef void (Processors::*InitFn)();
+  typedef int16_t (Processors::*ProcessSingleSampleFn)(uint8_t);
+  typedef void (Processors::*FillBufferFn)();
   typedef void (Processors::*ConfigureFn)(uint16_t*, ControlMode);
-  
+
   struct ProcessorCallbacks {
     InitFn init_fn;
     ProcessSingleSampleFn process_single_sample;
     FillBufferFn fill_buffer;
     ConfigureFn configure;
   };
-  
+
   inline void set_control_mode(ControlMode control_mode) {
     control_mode_ = control_mode;
     Configure();
   }
-  
+
   inline void set_parameter(uint8_t index, uint16_t parameter) {
     parameter_[index] = parameter;
     Configure();
   }
-  
+
   inline void CopyParameters(uint16_t* parameters, uint16_t size) {
     std::copy(&parameters[0], &parameters[size], &parameter_[0]);
   }
-  
+
   inline void set_function(ProcessorFunction function) {
     function_ = function;
-    lfo_.set_sync(function == PROCESSOR_FUNCTION_TAP_LFO);
+    //lfo_.set_sync(function == PROCESSOR_FUNCTION_TAP_LFO);
     callbacks_ = callbacks_table_[function];
-    if (function != PROCESSOR_FUNCTION_TAP_LFO) {
-      (this->*callbacks_.init_fn)();
-    }
+    //if (function != PROCESSOR_FUNCTION_TAP_LFO) {
+    //  (this->*callbacks_.init_fn)();
+    //}
     Configure();
   }
-  
+
   inline ProcessorFunction function() const { return function_; }
 
   inline int16_t Process(uint8_t control) {
@@ -149,7 +146,7 @@ class Processors {
       return output_buffer_.ImmediateRead();
     }
   }
-  
+
   inline bool Buffer() {
     if (callbacks_.fill_buffer) {
       if (output_buffer_.writable() < kBlockSize) {
@@ -162,9 +159,9 @@ class Processors {
       return true;
     }
   }
-  
+
   inline const NumberStation& number_station() const { return number_station_; }
-  
+
  private:
   void Configure() {
     if (function_ == PROCESSOR_FUNCTION_SNARE_DRUM ||
@@ -185,32 +182,32 @@ class Processors {
     }
     (this->*callbacks_.configure)(&parameter_[0], control_mode_);
   }
-  
+
   InputBuffer input_buffer_;
   OutputBuffer output_buffer_;
-  
+
   ControlMode control_mode_;
   ProcessorFunction function_;
   uint16_t parameter_[4];
-  
+
   ProcessorCallbacks callbacks_;
   static const ProcessorCallbacks callbacks_table_[PROCESSOR_FUNCTION_LAST];
-  
-  DECLARE_UNBUFFERED_PROCESSOR(MultistageEnvelope, envelope_);
-  DECLARE_BUFFERED_PROCESSOR(Lfo, lfo_);
+
+  //DECLARE_UNBUFFERED_PROCESSOR(MultistageEnvelope, envelope_);
+  //DECLARE_BUFFERED_PROCESSOR(Lfo, lfo_);
+  DECLARE_UNBUFFERED_PROCESSOR(WhiteNoise, white_noise_);
+  DECLARE_UNBUFFERED_PROCESSOR(PinkNoise, pink_noise_);
+  DECLARE_UNBUFFERED_PROCESSOR(HHNoise, hh_noise_);
   DECLARE_UNBUFFERED_PROCESSOR(BassDrum, bass_drum_);
   DECLARE_UNBUFFERED_PROCESSOR(SnareDrum, snare_drum_);
   DECLARE_UNBUFFERED_PROCESSOR(HighHat, high_hat_);
   DECLARE_BUFFERED_PROCESSOR(FmDrum, fm_drum_);
-  DECLARE_UNBUFFERED_PROCESSOR(HHNoise, hh_noise_);
-  DECLARE_UNBUFFERED_PROCESSOR(PinkNoise, pink_noise_);
-  DECLARE_UNBUFFERED_PROCESSOR(WhiteNoise, white_noise_);
   DECLARE_BUFFERED_PROCESSOR(PulseShaper, pulse_shaper_);
   DECLARE_BUFFERED_PROCESSOR(PulseRandomizer, pulse_randomizer_);
   DECLARE_UNBUFFERED_PROCESSOR(BouncingBall, bouncing_ball_);
   DECLARE_UNBUFFERED_PROCESSOR(MiniSequencer, mini_sequencer_);
   DECLARE_BUFFERED_PROCESSOR(NumberStation, number_station_);
-  
+
   DISALLOW_COPY_AND_ASSIGN(Processors);
 };
 

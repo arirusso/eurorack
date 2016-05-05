17bed1552de9e0bc6e41099546b8bbc9d6af2c01
Ari Russo <ari.russo@gmail.com>
2016-05-05 09:22:56 -0400

    first pass at decoupling functions from ui

--- peaks/ui.cc
+++ peaks/ui.cc
@@ -8,10 +8,10 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,7 +19,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-// 
+//
 // See http://creativecommons.org/licenses/MIT/ for more information.
 //
 // -----------------------------------------------------------------------------
@@ -56,6 +56,32 @@ const ProcessorFunction Ui::function_table_[FUNCTION_LAST][2] = {
 
 Storage<0x8020000, 16> storage;
 
+void Ui::NormalInit() {
+  edit_mode_ = EDIT_MODE_TWIN;
+  function_[0] = FUNCTION_ENVELOPE;
+  function_[1] = FUNCTION_ENVELOPE;
+  settings_.snap_mode = false;
+}
+
+void Ui::ParsimoniousInit() {
+  edit_mode_ = static_cast<EditMode>(settings_.edit_mode);
+  function_[0] = static_cast<Function>(settings_.function[0]);
+  function_[1] = static_cast<Function>(settings_.function[1]);
+  copy(&settings_.pot_value[0], &settings_.pot_value[8], &pot_value_[0]);
+
+  if (edit_mode_ == EDIT_MODE_FIRST || edit_mode_ == EDIT_MODE_SECOND) {
+    LockPots();
+    for (uint8_t i = 0; i < 4; ++i) {
+      processors[0].set_parameter(
+          i,
+          static_cast<uint16_t>(pot_value_[i]) << 8);
+      processors[1].set_parameter(
+          i,
+          static_cast<uint16_t>(pot_value_[i + 4]) << 8);
+    }
+  }
+}
+
 void Ui::Init() {
   leds_.Init();
   switches_.Init();
@@ -66,36 +92,18 @@ void Ui::Init() {
   fill(&adc_threshold_[0], &adc_threshold_[kNumAdcChannels], 0);
   fill(&snapped_[0], &snapped_[kNumAdcChannels], false);
   panel_gate_state_ = 0;
-  
+
   if (!storage.ParsimoniousLoad(&settings_, &version_token_)) {
-    edit_mode_ = EDIT_MODE_TWIN;
-    function_[0] = FUNCTION_ENVELOPE;
-    function_[1] = FUNCTION_ENVELOPE;
-    settings_.snap_mode = false;
+    NormalInit();
   } else {
-    edit_mode_ = static_cast<EditMode>(settings_.edit_mode);
-    function_[0] = static_cast<Function>(settings_.function[0]);
-    function_[1] = static_cast<Function>(settings_.function[1]);
-    copy(&settings_.pot_value[0], &settings_.pot_value[8], &pot_value_[0]);
-    
-    if (edit_mode_ == EDIT_MODE_FIRST || edit_mode_ == EDIT_MODE_SECOND) {
-      LockPots();
-      for (uint8_t i = 0; i < 4; ++i) {
-        processors[0].set_parameter(
-            i,
-            static_cast<uint16_t>(pot_value_[i]) << 8);
-        processors[1].set_parameter(
-            i,
-            static_cast<uint16_t>(pot_value_[i + 4]) << 8);
-      }
-    }
+    ParsimoniousInit();
   }
-  
+
   if (switches_.pressed_immediate(SWITCH_TWIN_MODE)) {
     settings_.snap_mode = !settings_.snap_mode;
     SaveState();
   }
-  
+
   ChangeControlMode();
   SetFunction(0, function_[0]);
   SetFunction(1, function_[1]);
@@ -118,6 +126,14 @@ void Ui::SaveState() {
   storage.ParsimoniousSave(settings_, &version_token_);
 }
 
+void Ui::RefreshLedsNumberStation(uint8_t b[]) {
+  leds_.set_pattern(
+      processors[0].number_station().digit() ^ \
+      processors[1].number_station().digit());
+  b[0] = processors[0].number_station().gate() ? 255 : 0;
+  b[1] = processors[1].number_station().gate() ? 255 : 0;
+}
+
 inline void Ui::RefreshLeds() {
   uint8_t flash = (system_clock.milliseconds() >> 7) & 7;
   switch (edit_mode_) {
@@ -137,7 +153,7 @@ inline void Ui::RefreshLeds() {
   } else {
     leds_.set_function(function() & 3);
   }
-  
+
   uint8_t b[2];
   for (uint8_t i = 0; i < 2; ++i) {
     switch (function_[i]) {
@@ -156,15 +172,11 @@ inline void Ui::RefreshLeds() {
         break;
     }
   }
-  
+
   if (processors[0].function() == PROCESSOR_FUNCTION_NUMBER_STATION) {
-    leds_.set_pattern(
-        processors[0].number_station().digit() ^ \
-        processors[1].number_station().digit());
-    b[0] = processors[0].number_station().gate() ? 255 : 0;
-    b[1] = processors[1].number_station().gate() ? 255 : 0;
+    RefreshLedsNumberStation(b);
   }
-  
+
   leds_.set_levels(b[0], b[1]);
 }
 
@@ -185,39 +197,55 @@ void Ui::PollPots() {
   }
 }
 
+void Ui::HandleSwitchesLongPress(uint8_t index, int32_t pressed_time) {
+  if (switches_.pressed(1 - index)) {
+    ++double_press_counter_;
+    press_time_[0] = press_time_[1] = 0;
+    if (double_press_counter_ == 3) {
+      double_press_counter_ = 0;
+      processors[0].set_function(PROCESSOR_FUNCTION_NUMBER_STATION);
+      processors[1].set_function(PROCESSOR_FUNCTION_NUMBER_STATION);
+    }
+  } else {
+    queue_.AddEvent(CONTROL_SWITCH, index, pressed_time);
+    press_time_[index] = 0;  // Inhibit next release event
+  }
+}
+
+void Ui::PollForSwitchLongPress(uint8_t index) {
+  if (switches_.pressed(index) && press_time_[index] != 0 && index < SWITCH_GATE_TRIG_1) {
+    int32_t pressed_time = system_clock.milliseconds() - press_time_[index];
+    if (pressed_time > kLongPressDuration) {
+      HandleSwitchesLongPress(index, pressed_time);
+    }
+  }
+}
+
+void Ui::PollForSwitchPress(uint8_t index) {
+  if (switches_.just_pressed(index)) {
+    queue_.AddEvent(CONTROL_SWITCH, index, 0);
+    press_time_[index] = system_clock.milliseconds();
+  }
+}
+
+void Ui::PollForSwitchRelease(uint8_t index) {
+  if (switches_.released(index) && press_time_[index] != 0) {
+    queue_.AddEvent(
+        CONTROL_SWITCH,
+        index,
+        system_clock.milliseconds() - press_time_[index] + 1);
+  }
+}
+
 void Ui::Poll() {
   system_clock.Tick();
   switches_.Debounce();
   for (uint8_t i = 0; i < kNumSwitches; ++i) {
-    if (switches_.just_pressed(i)) {
-      queue_.AddEvent(CONTROL_SWITCH, i, 0);
-      press_time_[i] = system_clock.milliseconds();
-    }
-    if (switches_.pressed(i) && press_time_[i] != 0 && i < SWITCH_GATE_TRIG_1) {
-      int32_t pressed_time = system_clock.milliseconds() - press_time_[i];
-      if (pressed_time > kLongPressDuration) {
-        if (switches_.pressed(1 - i)) {
-          ++double_press_counter_;
-          press_time_[0] = press_time_[1] = 0;
-          if (double_press_counter_ == 3) {
-            double_press_counter_ = 0;
-            processors[0].set_function(PROCESSOR_FUNCTION_NUMBER_STATION);
-            processors[1].set_function(PROCESSOR_FUNCTION_NUMBER_STATION);
-          }
-        } else {
-          queue_.AddEvent(CONTROL_SWITCH, i, pressed_time);
-          press_time_[i] = 0;  // Inhibit next release event
-        }
-      }
-    }
-    if (switches_.released(i) && press_time_[i] != 0) {
-      queue_.AddEvent(
-          CONTROL_SWITCH,
-          i,
-          system_clock.milliseconds() - press_time_[i] + 1);
-    }
+    PollForSwitchPress(i);
+    PollForSwitchLongPress(i);
+    PollForSwitchRelease(i);
   }
-  
+
   RefreshLeds();
   leds_.Write();
 }
@@ -230,88 +258,118 @@ void Ui::OnSwitchPressed(const Event& e) {
   switch (e.control_id) {
     case SWITCH_TWIN_MODE:
       break;
-      
+
     case SWITCH_FUNCTION:
       break;
-      
+
     case SWITCH_GATE_TRIG_1:
       panel_gate_control_[0] = true;
       break;
-    
+
     case SWITCH_GATE_TRIG_2:
       panel_gate_control_[1] = true;
       break;
   }
 }
 
+void Ui::EnableSplitMode() {
+  processors[0].CopyParameters(&adc_value_[0], 2);
+  processors[1].CopyParameters(&adc_value_[2], 2);
+  processors[0].set_control_mode(CONTROL_MODE_HALF);
+  processors[1].set_control_mode(CONTROL_MODE_HALF);
+}
+
+void Ui::EnableTwinMode() {
+  processors[0].CopyParameters(&adc_value_[0], 4);
+  processors[1].CopyParameters(&adc_value_[0], 4);
+  processors[0].set_control_mode(CONTROL_MODE_FULL);
+  processors[1].set_control_mode(CONTROL_MODE_FULL);
+}
+
+void Ui::EnableFullMode() {
+  processors[0].set_control_mode(CONTROL_MODE_FULL);
+  processors[1].set_control_mode(CONTROL_MODE_FULL);
+}
+
 void Ui::ChangeControlMode() {
-  if (edit_mode_ == EDIT_MODE_SPLIT) {
-    processors[0].CopyParameters(&adc_value_[0], 2);
-    processors[1].CopyParameters(&adc_value_[2], 2);
-    processors[0].set_control_mode(CONTROL_MODE_HALF);
-    processors[1].set_control_mode(CONTROL_MODE_HALF);
-  } else if (edit_mode_ == EDIT_MODE_TWIN) {
-    processors[0].CopyParameters(&adc_value_[0], 4);
-    processors[1].CopyParameters(&adc_value_[0], 4);
-    processors[0].set_control_mode(CONTROL_MODE_FULL);
-    processors[1].set_control_mode(CONTROL_MODE_FULL);
-  } else {
-    processors[0].set_control_mode(CONTROL_MODE_FULL);
-    processors[1].set_control_mode(CONTROL_MODE_FULL);
+  switch (edit_mode_) {
+    case EDIT_MODE_SPLIT:
+      EnableSplitMode();
+      break;
+    case EDIT_MODE_TWIN:
+      EnableTwinMode();
+      break;
+    default:
+      EnableFullMode();
+      break;
   }
+}
+
+void Ui::SetFunctionSplitOrTwinModes(Function f) {
+  function_[0] = function_[1] = f;
+  processors[0].set_function(function_table_[f][0]);
+  processors[1].set_function(function_table_[f][1]);
 }
 
 void Ui::SetFunction(uint8_t index, Function f) {
   if (edit_mode_ == EDIT_MODE_SPLIT || edit_mode_ == EDIT_MODE_TWIN) {
-    function_[0] = function_[1] = f;
-    processors[0].set_function(function_table_[f][0]);
-    processors[1].set_function(function_table_[f][1]);
+    SetFunctionSplitOrTwinModes(f);
   } else {
     function_[index] = f;
     processors[index].set_function(function_table_[f][index]);
   }
 }
 
+void Ui::HandleLongPressModeSwitchRelease() {
+  edit_mode_ = static_cast<EditMode>(
+      (edit_mode_ + EDIT_MODE_FIRST) % EDIT_MODE_LAST);
+  function_[0] = function_[1];
+  processors[0].set_function(function_table_[function_[0]][0]);
+  processors[1].set_function(function_table_[function_[0]][1]);
+  LockPots();
+}
+
+void Ui::HandleNormalModeSwitchRelease() {
+  if (edit_mode_ <= EDIT_MODE_SPLIT) {
+    edit_mode_ = static_cast<EditMode>(EDIT_MODE_SPLIT - edit_mode_);
+  } else {
+    edit_mode_ = static_cast<EditMode>(EDIT_MODE_SECOND - (edit_mode_ & 1));
+    LockPots();
+  }
+}
+
+void Ui::HandleFunctionSwitchRelease(const Event& e) {
+  Function f = function();
+  if (e.data > kLongPressDuration) {
+    f = static_cast<Function>((f + FUNCTION_FIRST_ALTERNATE_FUNCTION) % FUNCTION_LAST);
+  } else {
+    if (f <= FUNCTION_DRUM_GENERATOR) {
+      f = static_cast<Function>((f + 1) & 3);
+    } else {
+      f = static_cast<Function>(((f + 1) & 3) + FUNCTION_FIRST_ALTERNATE_FUNCTION);
+    }
+  }
+  SetFunction(edit_mode_ - EDIT_MODE_FIRST, f);
+  SaveState();
+}
+
 void Ui::OnSwitchReleased(const Event& e) {
   switch (e.control_id) {
     case SWITCH_TWIN_MODE:
       if (e.data > kLongPressDuration) {
-        edit_mode_ = static_cast<EditMode>(
-            (edit_mode_ + EDIT_MODE_FIRST) % EDIT_MODE_LAST);
-        function_[0] = function_[1];
-        processors[0].set_function(function_table_[function_[0]][0]);
-        processors[1].set_function(function_table_[function_[0]][1]);
-        LockPots();
+        HandleLongPressModeSwitchRelease();
       } else {
-        if (edit_mode_ <= EDIT_MODE_SPLIT) {
-          edit_mode_ = static_cast<EditMode>(EDIT_MODE_SPLIT - edit_mode_);
-        } else {
-          edit_mode_ = static_cast<EditMode>(EDIT_MODE_SECOND - (edit_mode_ & 1));
-          LockPots();
-        }
+        HandleNormalModeSwitchRelease();
       }
-      
+
       ChangeControlMode();
       SaveState();
       break;
-      
+
     case SWITCH_FUNCTION:
-      {
-        Function f = function();
-        if (e.data > kLongPressDuration) {
-          f = static_cast<Function>((f + FUNCTION_FIRST_ALTERNATE_FUNCTION) % FUNCTION_LAST);
-        } else {
-          if (f <= FUNCTION_DRUM_GENERATOR) {
-            f = static_cast<Function>((f + 1) & 3);
-          } else {
-            f = static_cast<Function>(((f + 1) & 3) + FUNCTION_FIRST_ALTERNATE_FUNCTION);
-          }
-        }
-        SetFunction(edit_mode_ - EDIT_MODE_FIRST, f);
-        SaveState();
-      }
+      HandleFunctionSwitchRelease(e);
       break;
-      
+
     case SWITCH_GATE_TRIG_1:
       panel_gate_control_[0] = false;
       break;
@@ -322,33 +380,41 @@ void Ui::OnSwitchReleased(const Event& e) {
   }
 }
 
+void Ui::HandleTwinModePotChange(const Event& e) {
+  processors[0].set_parameter(e.control_id, e.data);
+  processors[1].set_parameter(e.control_id, e.data);
+  pot_value_[e.control_id] = e.data >> 8;
+}
+
+void Ui::HandleSplitModePotChange(const Event& e) {
+  if (e.control_id < 2) {
+    processors[0].set_parameter(e.control_id, e.data);
+  } else {
+    processors[1].set_parameter(e.control_id - 2, e.data);
+  }
+  pot_value_[e.control_id] = e.data >> 8;
+}
+
 void Ui::OnPotChanged(const Event& e) {
   switch (edit_mode_) {
     case EDIT_MODE_TWIN:
-      processors[0].set_parameter(e.control_id, e.data);
-      processors[1].set_parameter(e.control_id, e.data);
-      pot_value_[e.control_id] = e.data >> 8;
+      HandleTwinModePotChange(e);
       break;
     case EDIT_MODE_SPLIT:
-      if (e.control_id < 2) {
-        processors[0].set_parameter(e.control_id, e.data);
-      } else {
-        processors[1].set_parameter(e.control_id - 2, e.data);
-      }
-      pot_value_[e.control_id] = e.data >> 8;
+      HandleSplitModePotChange(e);
       break;
     case EDIT_MODE_FIRST:
     case EDIT_MODE_SECOND:
       {
         uint8_t index = e.control_id + (edit_mode_ - EDIT_MODE_FIRST) * 4;
         Processors* p = &processors[edit_mode_ - EDIT_MODE_FIRST];
-        
+
         int16_t delta = static_cast<int16_t>(pot_value_[index]) - \
             static_cast<int16_t>(e.data >> 8);
         if (delta < 0) {
           delta = -delta;
         }
-        
+
         if (!settings_.snap_mode || snapped_[e.control_id] || delta <= 2) {
           p->set_parameter(e.control_id, e.data);
           pot_value_[index] = e.data >> 8;
@@ -361,17 +427,26 @@ void Ui::OnPotChanged(const Event& e) {
   }
 }
 
+void Ui::OnSwitchTouched(Event e) {
+  if (e.data == 0) {
+    OnSwitchPressed(e);
+  } else {
+    OnSwitchReleased(e);
+  }
+}
+
 void Ui::DoEvents() {
   while (queue_.available()) {
     Event e = queue_.PullEvent();
-    if (e.control_type == CONTROL_SWITCH) {
-      if (e.data == 0) {
-        OnSwitchPressed(e);
-      } else {
-        OnSwitchReleased(e);
-      }
-    } else if (e.control_type == CONTROL_POT) {
-      OnPotChanged(e);
+    switch (e.control_type) {
+      case CONTROL_SWITCH:
+        OnSwitchTouched(e);
+        break;
+      case CONTROL_POT:
+        OnPotChanged(e);
+        break;
+      default:
+        break;
     }
   }
   if (queue_.idle_time() > 1000) {
